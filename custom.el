3a73889881f7d15afef5d526e6a04695123f47c1
dapinkone <dap@nullcorp.org>
2019-11-05 07:22:40 -0800

    major overhaul. forget this org-mode `literate programming` stuff. just do it in elisp with comments.

--- custom.el
+++ custom.el
@@ -3,119 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   ["#1B2229" "#BF616A" "#A3BE8C" "#ECBE7B" "#8FA1B3" "#c678dd" "#46D9FF" "#DFDFDF"])
- '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\2" t))))
- '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups"))))
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#657b83")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes afternoon)
- '(custom-safe-themes
-   (quote
-    ("2540689fd0bc5d74c4682764ff6c94057ba8061a98be5dd21116bf7bf301acfb" "aa0a998c0aa672156f19a1e1a3fb212cdc10338fb50063332a0df1646eb5dfea" "a4d03266add9a1c8f12b5309612cbbf96e1291773c7bc4fb685bfdaf83b721c6" "0a3a41085c19d8121ed0ad3eb658a475ccb948a70a83604641ee7d4c3575a4d5" "554b7f0439155d6eb648d4837ef03902f51124cacee021217e76f39e9dd314c2" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "2e1d19424153d41462ad31144549efa41f55dacda9b76571f73904612b15fd0a" "a7e7804313dbf827a441c86a8109ef5b64b03011383322cbdbf646eb02692f76" "d0404bd38534a00ee72a4f887a987d6bff87f4cf8d8f85149e32849b262465a5" "77bddca0879cb3b0ecdf071d9635c818827c57d69164291cb27268ae324efa84" "b550fc3d6f0407185ace746913449f6ed5ddc4a9f0cf3be218af4fb3127c7877" "3481e594ae6866d72c40ad77d86a1ffa338d01daa9eb0977e324f365cef4f47c" "5b6b9c2f0121417faa3f69924b2643656cd429044f956bfa5328000b21d78dc9" "9b402e9e8f62024b2e7f516465b63a4927028a7055392290600b776e4a5b9905" default)))
- '(desktop-save-mode nil)
- '(doom-one-brighter-comments t)
- '(electric-pair-mode nil)
- '(elpy-rpc-backend "jedi")
- '(elpy-test-discover-runner-command (quote ("pipenv" "run" "py.test")))
- '(elpy-test-runner (quote elpy-test-pytest-runner))
- '(fci-rule-color "#65737E")
- '(global-linum-mode nil)
- '(global-nlinum-mode nil)
- '(global-nlinum-relative-mode t)
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#fdf6e3" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#586e75")
- '(highlight-tail-colors
-   (quote
-    (("#eee8d5" . 0)
-     ("#B4C342" . 20)
-     ("#69CABF" . 30)
-     ("#69B7F0" . 50)
-     ("#DEB542" . 60)
-     ("#F2804F" . 70)
-     ("#F771AC" . 85)
-     ("#eee8d5" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
- '(hl-fg-colors
-   (quote
-    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
- '(indent-tabs-mode nil)
- '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#D08770"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A3BE8C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#4f5b66"))
- '(magit-diff-use-overlays nil)
- '(nlinum-format "%d")
- '(nlinum-use-right-margin t)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
- '(org-fontify-done-headline t)
- '(org-fontify-quote-and-verse-blocks t)
- '(org-fontify-whole-heading-line t)
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(package-selected-packages
    (quote
-    (exec-path-from-shell eglot lsp-java java-imports java-snippets javadoc-lookup csv-mode jinja2-mode highlight-indent-guides ac-racer undo-tree rainbow-delimiters racer flycheck-rust cargo rust-mode org-agenda-property counsel swiper ivy powerline badger-theme column-enforce-mode web-mode eyebrowse org-bullets afternoon-theme smex magit expand-region elpy ace-window)))
- '(password-cache-expiry 36000)
- '(pos-tip-background-color "#eee8d5")
- '(pos-tip-foreground-color "#586e75")
- '(python-shell-interpreter "python3")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
- '(term-default-bg-color "#fdf6e3")
- '(term-default-fg-color "#657b83")
- '(tramp-auto-save-directory "~/.emacs.d/tramp-autosaves" nil (tramp))
- '(vc-annotate-background "#1B2229")
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#A3BE8C")
-    (cons 40 "#bbbe86")
-    (cons 60 "#d3be80")
-    (cons 80 "#ECBE7B")
-    (cons 100 "#e2ab77")
-    (cons 120 "#d99973")
-    (cons 140 "#D08770")
-    (cons 160 "#cc8294")
-    (cons 180 "#c97db8")
-    (cons 200 "#c678dd")
-    (cons 220 "#c370b6")
-    (cons 240 "#c16890")
-    (cons 260 "#BF616A")
-    (cons 280 "#a35f69")
-    (cons 300 "#875e68")
-    (cons 320 "#6b5c67")
-    (cons 340 "#65737E")
-    (cons 360 "#65737E")))
- '(vc-annotate-very-old-color nil)
- '(web-mode-auto-close-style 2)
- '(web-mode-enable-auto-expanding t)
- '(weechat-color-list
-   (quote
-    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
- '(winner-mode t)
- '(xterm-color-names
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
- '(xterm-color-names-bright
-   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+    (highlight-indentation python-black flycheck flymake-python-pyflakes flymake-rust rust-mode elpy rainbow-delimiters ace-window org-bullets)))
+ '(recentf-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(avy-lead-face ((t (:background "color-21" :foreground "white"))))
- '(avy-lead-face-1 ((t (:background "brightblack" :foreground "white"))))
- '(avy-lead-face-2 ((t (:background "color-22" :foreground "white"))))
- '(nlinum-relative-current-face ((t (:inherit linum :background "black" :foreground "red" :weight bold))))
- '(org-level-1 ((t (:inherit outline-1 :weight bold)))))
-(put 'erase-buffer 'disabled nil)
+ )
--- org-settings.el
+++ org-settings.el
@@ -1,21 +1,21 @@
 ;; GTD org agenda stuff.
 ;; credit where due:
 ;; https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
-(setq org-agenda-files '("~/gtd/inbox.org"
-                         "~/gtd/gtd.org"
-                         "~/gtd/tickler.org"))
+(setq org-agenda-files '("~/s/gtd/inbox.org"
+                         "~/s/gtd/gtd.org"
+                         "~/s/gtd/tickler.org"))
 
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
-                               (file+headline "~/gtd/inbox.org" "Tasks")
+                               (file+headline "~/s/gtd/inbox.org" "Tasks")
                                "* TODO %i%?")
                               ("T" "Tickler" entry
-                               (file+headline "~/gtd/tickler.org" "Tickler")
+                               (file+headline "~/s/gtd/tickler.org" "Tickler")
                                "* %i%? \n %U")))
 
 ;; what is (max)level doing here?
-(setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
-                           ("~/gtd/someday.org" :level .1)
-                           ("~/gtd/tickler.org" :maxlevel .2)))
+(setq org-refile-targets '(("~/s/gtd/gtd.org" :maxlevel . 3)
+                           ("~/s/gtd/someday.org" :level .1)
+                           ("~/s/gtd/tickler.org" :maxlevel .2)))
 
 (setq org-todo-keywords
    (quote
@@ -49,3 +49,10 @@
 
 ;; hide special chars such as / in /italics/ and * in *bold*
 (setq org-hide-emphasis-markers t)
+
+;; TODO: add hooks for org-agenda-list to update
+;; particularly window-configuration-change-hook
+
+
+;; updates org-agenda every 5 minutes
+;; (run-with-timer 0 300 (lambda () (org-agenda nil "a")) )
