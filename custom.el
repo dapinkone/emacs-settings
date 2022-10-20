23c50c70986c2d1ea6e5f7b46b36bc6172face76
Dapinkone <peter.iriarte24@gmail.com>
2022-10-19 19:08:04 -0700

    updates

--- custom.el
+++ custom.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
 	["#01323d" "#ec423a" "#93a61a" "#c49619" "#3c98e0" "#e2468f" "#3cafa5" "#60767e"])
- '(custom-enabled-themes '(dracula))
+ '(custom-enabled-themes '(tango-dark))
  '(custom-safe-themes
 	'("1436985fac77baf06193993d88fa7d6b358ad7d600c1e52d12e64a2f07f07176" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
  '(package-selected-packages
-	'(fira-code-mode dracula-theme solarized-theme rainbow-delimiters elpy csharp-mode format-all erlang docker docker-compose-mode docker-tramp dockerfile-mode undo-tree magit lsp-ui json-mode lsp-mode go-guru go-eldoc flymake-python-pyflakes flycheck python-black go-tag go-impl go-mode go-playground go-rename avy ace-window org-bullets use-package))
+	'(typescript-mode docker-compose-mode fira-code-mode dracula-theme solarized-theme rainbow-delimiters elpy csharp-mode format-all erlang docker docker-tramp dockerfile-mode undo-tree magit lsp-ui json-mode lsp-mode go-guru go-eldoc flymake-python-pyflakes flycheck python-black go-tag go-impl go-mode go-playground go-rename avy ace-window org-bullets use-package))
  '(python-shell-exec-path nil)
  '(python-shell-interpreter "python3")
  '(recentf-mode t))
